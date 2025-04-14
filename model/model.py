a1fdf9b8d594c3b3a42c85564455843bce785e85
GioCasati <g.casati03@gmail.com>
2025-04-14 10:28:33 +0200

    Primo tentativo di soluzione

--- model/model.py
+++ model/model.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 
 class Model:
     def __init__(self):
-        self._minimum_cost_pattern = []
+        #self._minimum_cost_pattern = [] non serve, current attempt fa il suo lavoro
         self._current_attempt = []
         self._min_cost = float('inf')
 
@@ -18,17 +18,46 @@ class Model:
         situations = MeteoDao.get_situations_month(month, 15)
         self._current_attempt = []
         self._min_cost = float('inf')
-        self._get_minimum_cost_pattern([], situations, 0)
+        self._get_minimum_cost_pattern([], deepcopy(situations), 0)
         for day in self._current_attempt:
             print(day)
         print(self._min_cost)
 
     def _get_minimum_cost_pattern(self, current_pattern, remaining, current_cost):
-        if len(current_pattern)==15:
+        if len(current_pattern) == 1:
             print()
+        if len(current_pattern) == 2:
+            print()
+        if len(current_pattern) == 3:
+            print()
+        if len(current_pattern) == 4:
+            print()
+        if len(current_pattern) == 5:
+            print()
+        if len(current_pattern) == 6:
+            print()
+        if len(current_pattern) == 7:
+            print()
+        if len(current_pattern) == 8:
+            print()
+        if len(current_pattern) == 9:
+            print()
+        if len(current_pattern) == 10:
+            print()
+        if len(current_pattern) == 11:
+            print()
+        if len(current_pattern) == 12:
+            print()
+        if len(current_pattern) == 13:
+            print()
+        if len(current_pattern) == 14:
+            print()
+        if len(current_pattern) == 15:
+            print()
+
         if not remaining:
             if current_cost < self._min_cost:
-                self._current_attempt = deepcopy(current_pattern)
+                self._current_attempt = deepcopy(current_pattern) # aggiunta qui deepcopy e sotto
                 self._min_cost = current_cost
         else:
             for option in remaining[0]:
@@ -40,7 +69,7 @@ class Model:
                         change = True
                     current_pattern.append(option)
                     current_cost += option.umidita
-                    new_remaining = remaining[1:]
+                    new_remaining = deepcopy(remaining[1:])  #aggiunta qui
                     self._get_minimum_cost_pattern(current_pattern, new_remaining, current_cost)
                     current_pattern.pop()
                     current_cost -= option.umidita
@@ -48,8 +77,8 @@ class Model:
                         current_cost -= 100
 
     def _get_alternatives(self, current_pattern, remaining, current_cost):
-        if current_cost > self._min_cost:
-            return []
+        #if current_cost > self._min_cost:
+         #   return []
         if not current_pattern:
             return remaining[0]
         elif len(current_pattern) < 3:
@@ -65,17 +94,25 @@ class Model:
                 else:
                     return []
             else:
-                options = remaining[0]
+                options = deepcopy(remaining[0]) # aggiunta qui
+                to_remove = []
                 if cnt_genova >= 6:
                     for option in options:
                         if option.localita == "Genova":
-                            options.remove(option)
+                            to_remove.append(option)
+                            
                 if cnt_milano >= 6:
                     for option in options:
                         if option.localita == "Milano":
-                            options.remove(option)
+                            to_remove.append(option)
+
                 if cnt_torino >= 6:
                     for option in options:
                         if option.localita == "Torino":
-                            options.remove(option)
+                            to_remove.append(option)
+
+                if len(to_remove) > 0:
+                    for option in to_remove:
+                        options.remove(option)
+
                 return options
