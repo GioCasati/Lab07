8b7ed37e9a317016b4e6a927ba32a8c0e784c0d2
GioCasati <g.casati03@gmail.com>
2025-04-14 11:46:41 +0200

    Completata l'interfaccia e snellito vincoli e ricorsione

--- UI/controller.py
+++ UI/controller.py
@@ -12,6 +12,7 @@ class Controller:
         self._model = model
         # other attributes
         self._mese = 0
+        self.__calendar = {1:"gennaio", 2:"febbraio", 3:"marzo", 4:"aprile", 5:"maggio", 6:"giugno", 7:"luglio", 8:"agosto", 9:"settembre", 10:"ottobre", 11:"novembre", 12:"dicembre"}
 
     def handle_umidita_media(self, e):
         self._view.lst_result.clean()
@@ -20,6 +21,7 @@ class Controller:
             self._view.create_alert("Select a month")
             self._view.update_page()
             return
+        self._view.lst_result.controls.append(ft.Text(f"Umidità medie per il mese di {self.__calendar[month]}:", weight=ft.FontWeight.BOLD, size=16))
         for record in self._model.get_mean_humidity(month):
             self._view.lst_result.controls.append(ft.Text(f"{record["Localita"]}: {record["Mean_humidity"]}"))
         self._view.update_page()
@@ -32,7 +34,11 @@ class Controller:
             self._view.create_alert("Select a month")
             self._view.update_page()
             return
-        self._model.get_sequence(month)
+        (pattern, cost) = self._model.get_optimal_sequence(month)
+        self._view.lst_result.controls.append(ft.Text(f"Pattern di minimo costo per il mese di {self.__calendar[month]}:", weight=ft.FontWeight.BOLD, size=16))
+        for day in pattern:
+            self._view.lst_result.controls.append(ft.Text(f"{day}"))
+        self._view.lst_result.controls.append(ft.Text(f"Costo complessivo: {cost}€", weight=ft.FontWeight.BOLD))
         self._view.update_page()
 
     def read_mese(self, e):
--- model/model.py
+++ model/model.py
@@ -7,112 +7,55 @@ from copy import deepcopy
 
 class Model:
     def __init__(self):
-        #self._minimum_cost_pattern = [] non serve, current attempt fa il suo lavoro
-        self._current_attempt = []
-        self._min_cost = float('inf')
+        self._optimal_pattern = []
+        self._optimal_cost = float('inf')
+
 
     def get_mean_humidity(self, month):
         return MeteoDao.get_mean_humidity(month)
 
-    def get_sequence(self, month):
+
+    def get_optimal_sequence(self, month):
         situations = MeteoDao.get_situations_month(month, 15)
-        self._current_attempt = []
-        self._min_cost = float('inf')
-        self._get_minimum_cost_pattern([], deepcopy(situations), 0)
-        for day in self._current_attempt:
-            print(day)
-        print(self._min_cost)
+        self._optimal_pattern = []
+        self._optimal_cost = float('inf')
+        self._recursion([], situations, 0)
+        return self._optimal_pattern, self._optimal_cost
 
-    def _get_minimum_cost_pattern(self, current_pattern, remaining, current_cost):
-        if len(current_pattern) == 1:
-            print()
-        if len(current_pattern) == 2:
-            print()
-        if len(current_pattern) == 3:
-            print()
-        if len(current_pattern) == 4:
-            print()
-        if len(current_pattern) == 5:
-            print()
-        if len(current_pattern) == 6:
-            print()
-        if len(current_pattern) == 7:
-            print()
-        if len(current_pattern) == 8:
-            print()
-        if len(current_pattern) == 9:
-            print()
-        if len(current_pattern) == 10:
-            print()
-        if len(current_pattern) == 11:
-            print()
-        if len(current_pattern) == 12:
-            print()
-        if len(current_pattern) == 13:
-            print()
-        if len(current_pattern) == 14:
-            print()
-        if len(current_pattern) == 15:
-            print()
 
-        if not remaining:
-            if current_cost < self._min_cost:
-                self._current_attempt = deepcopy(current_pattern) # aggiunta qui deepcopy e sotto
-                self._min_cost = current_cost
+    def _recursion(self, current_pattern, situations, current_cost):
+        if len(current_pattern) == len(situations):
+            if current_cost < self._optimal_cost:
+                self._optimal_pattern = deepcopy(current_pattern) # aggiunta qui deepcopy e sotto
+                self._optimal_cost = current_cost
         else:
-            for option in remaining[0]:
-                options = self._get_alternatives(current_pattern, remaining, current_cost)
-                if option in options:
+            for option in situations[len(current_pattern)]:
+                if self._is_admissible(current_pattern, option, current_cost):
                     change = False
                     if len(current_pattern) > 0 and option.localita != current_pattern[-1].localita:
                         current_cost += 100
                         change = True
                     current_pattern.append(option)
                     current_cost += option.umidita
-                    new_remaining = deepcopy(remaining[1:])  #aggiunta qui
-                    self._get_minimum_cost_pattern(current_pattern, new_remaining, current_cost)
+                    self._recursion(current_pattern, situations, current_cost)
                     current_pattern.pop()
                     current_cost -= option.umidita
                     if change:
                         current_cost -= 100
 
-    def _get_alternatives(self, current_pattern, remaining, current_cost):
-        #if current_cost > self._min_cost:
-         #   return []
-        if not current_pattern:
-            return remaining[0]
-        elif len(current_pattern) < 3:
-            return [s for s in remaining[0] if s.localita == current_pattern[-1].localita]
+
+    def _is_admissible(self, current_pattern, option, current_cost):
+        if current_cost > self._optimal_cost:
+            return False
+        if 0 < len(current_pattern) < 3:
+            return option.localita == current_pattern[-1].localita
+        elif len(current_pattern) >= 3:
+            counter = 0
+            for day in current_pattern:
+                if day.localita == option.localita:
+                    counter += 1
+            return (counter < 6 and
+                    (current_pattern[-1].localita == current_pattern[-2].localita == current_pattern[-3].localita or
+                     option.localita == current_pattern[-1].localita))
         else:
-            cnt_genova = len([s for s in current_pattern if s.localita == "Genova"])
-            cnt_milano = len([s for s in current_pattern if s.localita == "Milano"])
-            cnt_torino = len([s for s in current_pattern if s.localita == "Torino"])
-            can_change = current_pattern[-1].localita == current_pattern[-2].localita == current_pattern[-3].localita
-            if not can_change:
-                if len([s for s in current_pattern if s.localita == current_pattern[-1].localita]) < 6:
-                    return [s for s in remaining[0] if s.localita == current_pattern[-1].localita]
-                else:
-                    return []
-            else:
-                options = deepcopy(remaining[0]) # aggiunta qui
-                to_remove = []
-                if cnt_genova >= 6:
-                    for option in options:
-                        if option.localita == "Genova":
-                            to_remove.append(option)
-                            
-                if cnt_milano >= 6:
-                    for option in options:
-                        if option.localita == "Milano":
-                            to_remove.append(option)
-
-                if cnt_torino >= 6:
-                    for option in options:
-                        if option.localita == "Torino":
-                            to_remove.append(option)
-
-                if len(to_remove) > 0:
-                    for option in to_remove:
-                        options.remove(option)
-
-                return options
+            return True
